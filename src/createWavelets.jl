6df50b5835bc56bd14b0a96b1b9687a3037d020e
Wai Ho CHAK <wchak@ucdavis.edu>
2021-05-22 04:43:55 -0700

    10 Edit Morse Wavelet: rescale om

--- src/createWavelets.jl
+++ src/createWavelets.jl
@@ -71,9 +71,11 @@ function mother(this::CWT{W,T,Morse,N}, s::Real, sWidth::Real,
     
     #  ω = LinRange(0,1-(1/len),len)
     
-    om = 2 * pi * ω./ fact / max(1, s);
+    # om = 2 * pi * ω./ fact / max(1, s);
 
-    om = (om .- minimum(om)) ./ (maximum(om) + sqrt(eps()) - minimum(om));
+    om = ω./ fact / max(1, s);
+
+    # om = (om .- minimum(om)) ./ (maximum(om) + sqrt(eps()) - minimum(om));
 
     if be == 0
         daughter = 2 * exp.(-om.^ga);
