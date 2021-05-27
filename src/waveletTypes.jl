a3015a884c5e568a897ebbd608a005e823c126be
Wai Ho CHAK <wchak@ucdavis.edu>
2021-05-27 03:35:51 -0700

    14 Edit Morse Wavelet

--- src/waveletTypes.jl
+++ src/waveletTypes.jl
@@ -34,7 +34,7 @@ function Morse_convert(ga::Real, be::Real, cf::Real)
     Morse(ga,be,cf)
 end
 
-Morse() = Morse_convert(3, 1, 1)
+Morse() = Morse_convert(3, 2, 1)
 class(::Morse) = "Morse"; name(::Morse) = "morse"; 
 vanishingmoments(::Morse) = 0; isAnalytic(::Morse) = true
 const morse = Morse()
