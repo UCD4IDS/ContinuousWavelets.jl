535d20896b48dae6d3389d09222600dc9e0b8b25
dsweber2 <david.weber2@protonmail.com>
2021-11-15 13:12:14 -0800

    linewidth on the README plots

--- docs/allInOne.jl
+++ docs/allInOne.jl
@@ -111,11 +111,10 @@ savefig("mothers.svg")#hide
 
 c = wavelet(Morlet(π), averagingType = NoAve(), β = 2);
 exs = cat(testfunction(n, "Doppler"), testfunction(n, "Blocks"), testfunction(n, "Bumps"), testfunction(n, "HeaviSine"), dims = 2)
-res = cwt(exs, c)
-p1 = plot(plot(exs[:, 1], legend = false, title = "Doppler", yticks = [], xticks = []), plot(exs[:, 2], legend = false, title = "Blocks", yticks = [], xticks = []), plot(exs[:, 3], legend = false, title = "Bumps", yticks = [], xticks = []), plot(exs[:, 4], legend = false, title = "HeaviSine", yticks = [], xticks = []), layout = (1, 4))
+p1 = plot(plot(exs[:, 1], legend = false, title = "Doppler", yticks = [], xticks = [], linewidth = 2), plot(exs[:, 2], legend = false, title = "Blocks", yticks = [], xticks = [], linewidth = 2), plot(exs[:, 3], legend = false, title = "Bumps", yticks = [], xticks = [], linewidth = 2), plot(exs[:, 4], legend = false, title = "HeaviSine", yticks = [], xticks = [], linewidth = 2), layout = (1, 4))
 c = wavelet(cDb2, β = 2, extraOctaves = -0);
-res = cwt(exs, c)
-p2 = plot(heatmap(identity.(res[:, :, 1])', xlabel = "time", xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 2])', xlabel = "time", xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 3])', xlabel = "time", xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 4])', xlabel = "time", xticks = false, yticks = [], c = :viridis, colorbar = false), layout = (1, 4))
+res = circshift(ContinuousWavelets.cwt(exs, c), (0, 1, 0))
+p2 = plot(heatmap(identity.(res[:, :, 1])', xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 2])', xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 3])', xticks = false, yticks = [], c = :viridis, colorbar = false), heatmap(identity.(res[:, :, 4])', xticks = false, yticks = [], c = :viridis, colorbar = false), layout = (1, 4))
 l = @layout [a{0.3h}; b{0.7h}]
 plot(p1, p2, layout = l)
 savefig("multiEx.svg")
