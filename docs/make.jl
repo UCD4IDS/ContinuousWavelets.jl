5687820f0656ead03d939214b8b10429abfb26ca
dsweber2 <david.weber2@protonmail.com>
2021-11-20 17:07:56 -0800

    plots in docs being weird again

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,22 @@
 using Documenter, ContinuousWavelets
+ENV["PLOTS_TEST"] = "true"
 ENV["GKSwstype"] = "100"
-makedocs(sitename="ContinuousWavelets.jl",
-         format=Documenter.HTML(),
-         authors="David Weber",
-         clean=true,
-         pages=[
-             "basic usage" => "index.md",
-             "Install" => "installation.md",
-             "CWT" => [
-                 "Available Wavelet Families" => "coreType.md",
-                 "CWT Construction" => "CWTConstruction.md",
-                 "Wavelet Frequency Spacing" => "spacing.md",
-                 "Boundary Conditions" => "bound.md",
-                 "Inversion" => "inverse.md"
-             ],
-         ])
+makedocs(sitename = "ContinuousWavelets.jl",
+    format = Documenter.HTML(),
+    authors = "David Weber",
+    clean = true,
+    pages = [
+        "basic usage" => "index.md",
+        "Install" => "installation.md",
+        "CWT" => [
+            "Available Wavelet Families" => "coreType.md",
+            "CWT Construction" => "CWTConstruction.md",
+            "Wavelet Frequency Spacing" => "spacing.md",
+            "Boundary Conditions" => "bound.md",
+            "Inversion" => "inverse.md"
+        ],
+    ])
 
 deploydocs(
-    repo="github.com/dsweber2/ContinuousWavelets.jl.git",
+    repo = "github.com/dsweber2/ContinuousWavelets.jl.git",
 )
